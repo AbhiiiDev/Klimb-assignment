70e285505fcda5bd52d7e4e4fcd62714b7345d7c
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-10-18 19:33:41 +0530

    Added search email functionality

--- klimb-react/src/main.jsx
+++ klimb-react/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import ManageUser from './pages/ManageUser.jsx';
+import UserForm from './forms/UserForm.jsx';
+
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<ManageUser/>
+  },
+{  path:"/create",
+  element:<UserForm/>
+},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router}>
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
--- klimb-react/src/pages/ManageUser.jsx
+++ klimb-react/src/pages/ManageUser.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const ManageUser = () => {
 
     const [users,setUsers]=useState([]);
+    const [searchEmail,setSearchEmail]=useState("");
+
     const BASE_URL=import.meta.env.VITE_BASE_URL;
 
     useEffect(()=>{
@@ -20,22 +23,26 @@ console.log(data);
 
 
 
+        const filteredUser=users.filter((user)=>user.email.toLowerCase().includes(searchEmail.toLowerCase()));
+
+
   return (
     <div className="container mx-auto p-6">
   <div className="bg-white shadow-md rounded-lg p-4">
-
+<h1 className='font-bold text-2xl'>Manage Users</h1>
     <div className="flex justify-between items-center mb-4">
       <input 
         type="text" 
         placeholder="Search for email" 
+        onChange={(e)=>setSearchEmail(e.target.value)}
         className="border rounded-lg p-2 w-full max-w-xs"
       />
-      <button className="bg-green-500 text-white p-2 rounded-lg flex items-center">
+      <Link to='/create' className="bg-green-500 text-white p-2 rounded-lg flex items-center">
         <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
         </svg>
         Add
-      </button>
+      </Link>
     </div>
 
     {/* Table */}
@@ -52,7 +59,7 @@ console.log(data);
         </thead>
         <tbody>
             {
-            users.map((user)=>(
+            filteredUser.map((user)=>(
                 <tr className='border-b'>
                       <td className="p-3">{user.firstName+" "+user.lastName}</td>
             <td className="p-3">{user.phone}</td>
